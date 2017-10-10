d19ba881fb112554e541ab290f718771095bba73
ShinTabata <32173095+ShinTabata@users.noreply.github.com>
2017-10-10 09:13:33 +0900

    Add files via upload

---
+++ compact9.py
@@ -0,0 +1,15 @@
+
+###
+
+hogeo=1
+for num in range(100):
+    hogehoge = (num**205)%10
+    hoge=hogeo+hogehoge
+    if hoge==8 :
+        print(num)
+        break
+    else :
+        hogeo+1
+        continue
+
+###
